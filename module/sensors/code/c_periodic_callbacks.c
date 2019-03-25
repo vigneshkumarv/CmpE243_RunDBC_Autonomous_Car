aefda3a4d44c5ed4282430c3befc7770f08adc17
Chong Hang Cheong <kelvin611@gmail.com>
2019-03-24 19:20:35 -0700

    Add Sensor Testing and Source Code

--- module/sensors/code/c_periodic_callbacks.c
+++ module/sensors/code/c_periodic_callbacks.c
@@ -10,18 +10,25 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "can.h"
+#include "light_sensor_read_and_send.h"
 
 bool c_period_init(void) {
   CAN_init(can1, 100, 64, 64, ((void*)0), ((void*)0));
   CAN_bypass_filter_accept_all_msgs();
   CAN_reset_bus(can1);
+
   return true;
 }
 
 bool c_period_reg_tlm(void) { return true; }
 
 void c_period_1Hz(uint32_t count) { (void)count; }
-void c_period_10Hz(uint32_t count) { (void)count; }
+
+void c_period_10Hz(uint32_t count) {
+    (void)count;
+    light_sensor_read_and_send();
+
+}
 
 void c_period_100Hz(uint32_t count) { (void)count; }
 void c_period_1000Hz(uint32_t count) {
--- module/sensors/code/light_sensor_read_and_send.c
+++ module/sensors/code/light_sensor_read_and_send.c
@@ -6,25 +6,22 @@
  */
 
 #include "light_sensor_read_and_send.h"
+#include "generated_can/generated_can.h"
+//#include "C:\Users\YUI\CMPE243\autonomous-car\shared_code\wrappers\ls_wrapper.h"
+#include "wrappers\ls_wrapper.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "can.h"
 
 void light_sensor_read_and_send() {
-  int light_value = light_sensor_getRawValue();
+  int light_value = ls_get_per();
   SENSOR_DATA_t SENSOR_cmd = {0};
   SENSOR_cmd.SENSOR_DATA_cmd = light_value;
 
   can_msg_t can_msg = {0};
-  dbc_msg_hdr_t dbc_encode_SENSOR_DATA(can_msg.data.bytes, &SENSOR_cmd);
+  dbc_msg_hdr_t msg_hdr = dbc_encode_SENSOR_DATA(can_msg.data.bytes, &SENSOR_cmd);
   can_msg.msg_id = msg_hdr.mid;
   can_msg.frame_fields.data_len = msg_hdr.dlc;
-
   CAN_tx(can1, &can_msg, 0);
 }
-
-/*	CAN_init(can1, 100, 64, 64, ((void*)0), ((void*)0));
-        CAN_bypass_filter_accept_all_msgs();
-        CAN_reset_bus(can1);
-        */
