8a7736de70b021429d1f79b249bbc142b96e9907
bharath_vyas <9595vyas@gmail.com>
2019-04-20 07:50:56 -0700

    UT for GPS code

--- module/geo/tests/test_gps.c
+++ module/geo/tests/test_gps.c
@@ -1,8 +1,8 @@
 #include "unity.h"
 
-#include "gps.h"
 #include "uart_wrapper.h"
-#include <stdio.h>
+#include "gps.h"
+#include "stdio.h"
 
 
 void setUp(void) {
@@ -10,8 +10,8 @@ void setUp(void) {
 void tearDown(void) {
 }
 
-static float gps_data_longitude = 0;
-static float gps_data_latitude = 0;
+static float gps_longitude = 37.336554;
+static float gps_latitude = -121.881848;
 static bool is_data_invalid = false;
 static float destination_latitude = 37.33485;
 static float destination_longitude = -121.880899;
@@ -19,10 +19,10 @@ static char delim[] = ",";
 
 void test_gps_module_init(void)
 {
-		uart2_init_IgnoreAndReturn(true);
+		uart2_init_ExpectAndReturn(true);
 		uart2_putLine_ExpectAndReturn(PMTK_SET_BAUDRATE_57600, 20, true);
 		uart2_init_ExpectAndReturn(57600, 256, 256, true);
-		
+		gps_module_init(void);
 }
 
 
@@ -55,7 +55,7 @@ void test_gps_obtain_and_process_data(void)
 
 void test_gps_get_distance(void)
 {
-	dounle lon_difference = 0;
+	double lon_difference = 0;
 	gps_get_bearing(gps_latitude, gps_longitude, destination_latitude, destination_longitude);
 	TEST_ASSERT_EQUAL_INT(0, gps_latitude);
 	TEST_ASSERT_EQUAL_INT(0, gps_longitude);
@@ -81,7 +81,31 @@ void test_gps_get_deflection_angle(void)
 	gps_get_deflection_angle(gps_bearing, compass_bearing);
 	TEST_ASSERT_EQUAL_INT(, deflection);
 	
+}
+
+void test_gps_obtain_data(void)
+{
+	int counter = 0;
 	
+	char data_array[100] = "$GPRMA,A,372011.6,N,1215254.7,W,,,ss.s,ccc,vv.v,W*hh";
+	
+	gps_module_get_data_Expect(data_array);
+	gps_obtain_data_Expect(void);
+	TEST_ASSERT_EQUAL_INT(6, counter);
+	TEST_ASSERT_EQUAL_INT(372011.6, gps_latitude);
+	TEST_ASSERT_EQUAL_INT(1215254.7, gps_longitude);
+	TEST_ASSERT_EQUAL_FALSE(is_data_invalid);
+	
+	data_array[100] = "$GPRMA,N,1215254.7,W,,,ss.s,ccc,vv.v,W*hh";
+	gps_module_get_data_Expect(data_array);
+	gps_obtain_data_Expect(void);
+	TEST_ASSERT_EQUAL_INT(3, counter);
+	TEST_ASSERT_EQUAL_INT(1215254.7, gps_latitude);
+	TEST_ASSERT_EQUAL_TRUE(is_data_invalid);
+	
+}
+
+
 	
 	
 	
