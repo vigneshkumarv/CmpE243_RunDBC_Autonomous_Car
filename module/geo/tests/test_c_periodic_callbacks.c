f6a35622fcb42433e94d196948389fdf936e3139
bharath_vyas <9595vyas@gmail.com>
2019-04-25 14:51:54 -0700

    UT for GPS and Compass

--- module/geo/tests/test_c_periodic_callbacks.c
+++ module/geo/tests/test_c_periodic_callbacks.c
@@ -11,16 +11,40 @@
 #include "Mockgps.h"
 #include "queue.h"
 
-can_t geo_module = {0};
+//can_t geo_module = {0};
 queue_S queue;
+uint32_t count = 0;
+//can_t geo_module = {0};
 
 void test_c_period_init(void)
 {
+    can_t geo_module = {0};
     can_initialization_Expect(&geo_module);
     gps_module_init_Expect();
 
-    TEST_ASSERT_TRUE(c_period_init());
-    TEST_ASSERT_EQUAL_INT(0, queue.front);
-    TEST_ASSERT_EQUAL_INT(-1, queue.rear);
-    TEST_ASSERT_EQUAL_INT(0, queue.size);
+    bool return_var = c_period_init();
+    TEST_ASSERT_TRUE(return_var);
+
+}
+
+void test_c_period_reg_tlm(void)
+{
+    bool return_var = c_period_reg_tlm();
+    TEST_ASSERT_TRUE(return_var);
+
+}
+
+void test_c_period_1Hz(void)
+{
+    can_t geo_module = {0};
+    send_heartbeat_msg_Expect(&geo_module);
+    c_period_1Hz(count);
+}
+
+void test_c_period_10Hz(void)
+{
+    can_t geo_module = {0};
+    gps_obtain_and_process_data_Expect(count);
+    send_can_msg_Expect(&geo_module, &geo_data);
+    c_period_10Hz(count);
 }
